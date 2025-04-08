13cf401b3e995229d0d147d9a8cee3e493eb388b
kerubifi <playkirill061@gmail.com>
2025-04-08 21:01:11 +0300

    Добавление поиска по названию игры

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
+import { useState } from 'react';
 import './App.css';
 import { games } from './Components/Games';
 import { Header } from './Components/Header';
 
 function App() {
+
+  const [inputName, setInputName] = useState('')
+
+  const filterArray = inputName ? games.filter(el => el.name.includes(inputName)) : games
+
+  const handleInput = (text) => {
+    setInputName(text)
+  }
+
   return (
     <div>
-      <Header/>
+      <Header handleInput={handleInput}/>
       <div className='cardsbox'>
-        {games.map((el) => (
+        {filterArray.map((el) => (
           <div className='cards'>
             <div><img src={el.img} alt={el.alt} width={30} /></div>
             <div>{el.name}</div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import logo from '../images/openfaas-dark.svg'
 
-export const Header = () => {
+export const Header = ({handleInput}) => {
     return (
         <div className='header'>
             <div>
                 <img src={logo} alt="logo" width={30} />
             </div>
+            <input onChange={(event => handleInput(event.target.value))} />
             <div>header</div>
         </div>
     )
